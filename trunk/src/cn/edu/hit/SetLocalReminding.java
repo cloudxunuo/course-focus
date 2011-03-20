f8809076821a85ac59221cf5cdb6fec026e1583f
cloudxunuo@gmail.com <cloudxunuo@gmail.com@c5f3b755-e28a-a4d4-412f-c08da0015ab9>
2011-03-20 13:16:58 +0000

    增加登录,设置本地提醒的activity

---
+++ trunk/src/cn/edu/hit/SetLocalReminding.java
@@ -0,0 +1,13 @@
+package cn.edu.hit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetLocalReminding extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setlocalreminding);
+	}
+}
